e6ca1185a7793d091e510f489966b0135be93acc
Jake Soenneker <jake@soenneker.com>
2026-04-15 04:36:08 -0500

    sharing

--- src/Soenneker.Playwrights.Session/BrowserSession.cs
+++ src/Soenneker.Playwrights.Session/BrowserSession.cs
@@ -7,18 +7,27 @@ namespace Soenneker.Playwrights.Session;
 ///<inheritdoc cref="IBrowserSession"/>
 public sealed class BrowserSession : IBrowserSession
 {
+    private readonly bool _ownsContext;
+    private readonly bool _ownsPage;
+
     public IBrowserContext Context { get; }
 
     public IPage Page { get; }
 
-    public BrowserSession(IBrowserContext context, IPage page)
+    public BrowserSession(IBrowserContext context, IPage page, bool ownsContext = true, bool ownsPage = true)
     {
         Context = context;
         Page = page;
+        _ownsContext = ownsContext;
+        _ownsPage = ownsPage;
     }
 
-    public ValueTask DisposeAsync()
+    public async ValueTask DisposeAsync()
     {
-        return Context.DisposeAsync();
+        if (_ownsPage && !Page.IsClosed)
+            await Page.CloseAsync();
+
+        if (_ownsContext)
+            await Context.DisposeAsync();
     }
 }
